f5abc906063cf3eaf6e4621bad48b39067addd0f
Gabor Recski <recski@mokk.bme.hu>
2015-03-02 17:18:41 +0100

    parsing with custom constraints now works! 	modified:   src/stanford_jython.py

--- src/stanford_jython.py
+++ src/stanford_jython.py
@@ -1,28 +1,80 @@
+import math
+import os
 import sys
 
-
-
 sys.path.append(sys.argv[1])
- 
-from java.io import CharArrayReader
-from edu.stanford.nlp import *
- 
-lp = parser.lexparser.LexicalizedParser('edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz')
-tlp = trees.PennTreebankLanguagePack()
-lp.setOptionFlags(["-maxLength", "80", "-retainTmpSubcategories"])
- 
-sentence = 'One of my favorite features of functional programming \
-languages is that you can treat functions like values.'
- 
-toke = tlp.getTokenizerFactory().getTokenizer(CharArrayReader(sentence));
-wordlist = toke.tokenize()
- 
-if (lp.parse(wordlist)):
-    parse = lp.getBestParse()
- 
-gsf = tlp.grammaticalStructureFactory()
-gs = gsf.newGrammaticalStructure(parse)
-tdl = gs.typedDependenciesCollapsed()
- 
-print parse.toString() 
-print tdl
+
+from edu.stanford.nlp.process import Morphology, PTBTokenizer, WordTokenFactory
+from edu.stanford.nlp.parser.common import ParserConstraint
+from edu.stanford.nlp.parser.lexparser import Options
+from edu.stanford.nlp.parser.lexparser import LexicalizedParser
+from edu.stanford.nlp.ling import Sentence
+from edu.stanford.nlp.trees import PennTreebankLanguagePack
+
+from java.io import StringReader
+from java.util.regex import Pattern
+
+class StanfordParser:
+
+    def __init__(self, parser_file,
+                 parser_options=['-maxLength', '80',
+                                 '-retainTmpSubcategories']):
+
+        """@param parser_file: path to the serialised parser model
+            (e.g. englishPCFG.ser.gz)
+        @param parser_options: options
+        """
+
+        assert os.path.exists(parser_file)
+        options = Options()
+        options.setOptions(parser_options)
+        self.lp = LexicalizedParser.getParserFromFile(parser_file, options)
+        tlp = PennTreebankLanguagePack()
+        self.gsf = tlp.grammaticalStructureFactory()
+        self.lemmer = Morphology()
+        self.word_token_factory = WordTokenFactory()
+        self.parser_query = None
+
+    def tokenize(self, text):
+        reader = StringReader(text)
+        tokeniser = PTBTokenizer(reader, self.word_token_factory, None)
+        tokens = tokeniser.tokenize()
+        return tokens
+
+    def get_parse(self, sentence):
+        tokens = [unicode(x) for x in self.tokenize(sentence)]
+        parse = self.lp.apply(Sentence.toWordList(tokens))
+        return parse
+
+    def get_grammatical_structure(self, parse):
+        return self.gsf.newGrammaticalStructure(parse)
+
+    def get_kbest(self, query, k=3):
+        for candidate_tree in query.getKBestPCFGParses(k):
+            parse = candidate_tree.object()
+            prob = math.e ** candidate_tree.score()
+            yield prob, parse
+
+def main():
+    parser_file = sys.argv[2]
+    parser = StanfordParser(parser_file)
+
+    query = parser.lp.parserQuery()
+    #constraints = [ParserConstraint(0, 3, Pattern.compile("VP.*"))]
+    constraints = [ParserConstraint(0, 12, Pattern.compile("NP.*"))]
+    query.setConstraints(constraints)
+
+    sentence = 'the size of a radio wave used to broadcast a radio signal'
+
+    toks = parser.tokenize(sentence)
+    query.parse(toks)
+    parse = query.getBestParse()
+
+    gs = parser.get_grammatical_structure(parse)
+    dependencies = gs.typedDependenciesCollapsed()
+
+    print parse.pennPrint()
+    print "\n".join(map(str, dependencies))
+
+if __name__ == "__main__":
+    main()
