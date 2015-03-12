47592a9e7d8b2c013eef40a812a4e3147910e644
Gabor Recski <recski@mokk.bme.hu>
2015-03-12 13:50:29 +0100

    this was never functional and we'll have something different that does its job soon 	deleted:    src/get_def_words.py

--- src/get_def_words.py
+++
@@ -1,9 +0,0 @@
-import sys
-from longman_parser import LongmanParser
-
-def main():
-    dictionary = LongmanParser.parse_file(sys.argv[1])
-    for entry in dictionary['entries']:
-        def_words = set()
-        for sense in entry['senses']:
-            def_words |= sense['definition']
