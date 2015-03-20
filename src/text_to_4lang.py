52039ce072b32ed4f67de65aeafa578d816bd835
Gabor Recski <recski@mokk.bme.hu>
2015-03-20 15:09:38 +0100

    going back to using my own stanford_wrapper, which will soon use corenlp in batch mode to allow coreference resolution accross sentence boundaries 	modified:   src/text_to_4lang.py

--- src/text_to_4lang.py
+++ src/text_to_4lang.py
@@ -1,14 +1,12 @@
 from ConfigParser import ConfigParser
-import jsonrpc
 import logging
 import os
-import simplejson
 import sys
 
 from pymachine.utils import MachineGraph
 from pymachine.wrapper import Wrapper as MachineWrapper
 
-#from stanford_wrapper import StanfordWrapper
+from stanford_wrapper import StanfordWrapper
 
 __LOGLEVEL__ = 'DEBUG'
 __MACHINE_LOGLEVEL__ = 'INFO'
@@ -20,25 +18,24 @@ class TextTo4lang():
         self.cfg = ConfigParser()
         self.cfg.read([default_cfg_file, cfg_file])
 
+    def get_coref_pairs(corefs):
+        pass
+        #return [[phrase.split()[head_index] for phrase,
+
     def process(self, stream):
         sens = [line.strip() for line in stream]
         logging.info("running parser...")
-        server = jsonrpc.ServerProxy(
-            jsonrpc.JsonRpc20(),
-            jsonrpc.TransportTcpIp(addr=("127.0.0.1", 8080)))
+        stanford_wrapper = StanfordWrapper(self.cfg)
+        parsed_sens = stanford_wrapper.parse_sentences(sens)
         logging.info("loading machine wrapper...")
         logging.getLogger().setLevel(__MACHINE_LOGLEVEL__)
         machine_cfg_file = os.path.join(self.cfg_dir, 'machine.cfg')
         wrapper = MachineWrapper(machine_cfg_file)
         logging.info("processing sentences...")
-        for i, sen in enumerate(sens):
-            parsed_sen = simplejson.loads(server.parse(sen))['sentences'][0]
+        for i, sen in enumerate(parsed_sens):
             with open('test/sens/sen_{0}.dep'.format(i), 'w') as f:
-                f.write("\n".join(("{0}({1}, {2})".format(*dep)
-                                   for dep in parsed_sen['dependencies'])))
-            words_to_machines = wrapper.get_machines_from_parsed_deps(
-                ((dep, (w1, -1), (w2, -1))
-                 for dep, w1, w2 in parsed_sen['dependencies']))
+                f.write("\n".join(sen['deps']))
+            words_to_machines = wrapper.get_machines_from_deps(sen['deps'])
             graph = MachineGraph.create_from_machines(
                 words_to_machines.values())
             with open('test/sens/graphs/sen_{0}.dot'.format(i), 'w') as f:
