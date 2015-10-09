b9338d3da7176d746c0989ed3a8c843517583036
Gabor Recski <recski@mokk.bme.hu>
2015-10-09 11:59:28 +0200

    some more fixes

--- src/fourlang/collins_parser.py
+++ src/fourlang/collins_parser.py
@@ -28,10 +28,10 @@ class CollinsParser():
  #            print line
  #            yield MagyarParser.parse_entry(line)
         for section in re.split('#[hH]', CollinsParser.get_text(input_file)):
-            try:
-                yield CollinsParser.parse_entry(section)
-            except:
-                logging.warning("parse failed on section: {0}".format(section))
+            # try:
+            yield CollinsParser.parse_entry(section)
+            # except:
+            #     logging.warning("parse failed on section: {0}".format(section))
 
     @staticmethod
     def pattern_obj(pattern):
@@ -59,11 +59,20 @@ class CollinsParser():
 #        entry = re.sub('\n', ' ', entry)
         for pattern in ['\n', '@n']:
             entry = re.sub(pattern, " ", entry)
+        alternate_forms = CollinsParser.get_alternate_forms(entry)
         for pattern in ['#\+', '@\.', '\?!', '#5\(.*?\)', '#5\[.*?\]',
                         'or #3[^ ]+']:  # '#3' another spelling
             entry = re.sub(pattern, "", entry)
         return {'hw': CollinsParser.get_hw(entry),
-            'senses': CollinsParser.get_senses(entry)}
+            'senses': CollinsParser.get_senses(entry),
+            'alternate_forms': alternate_forms}
+
+    @staticmethod
+    def get_alternate_forms(entry):
+        forms = re.findall('#3(.*?)#[56]', entry)
+        return [
+            form.replace('#+', '').replace('@.', '').replace('#4', '').strip()
+            for form in forms]
 
     @staticmethod
     def get_pos(entry):
@@ -77,7 +86,8 @@ class CollinsParser():
     @staticmethod
     def get_hw(entry):
 #        print 'entry: ' + entry
-        return re.search('(.+?)#[56]', entry, re.S).group(1).strip()
+        return re.search('(.+?)#[56]', entry, re.S).group(1).replace(
+            '#4', '').strip()
 
     @staticmethod
     def get_senses(entry):
@@ -114,22 +124,26 @@ class CollinsParser():
 ##                 'pos': CollinsParser.get_pos(description)}]
 #                 'pos': CollinsParser.get_pos_from_sense(description)}]
 
+    pos_and_def_patt = re.compile(
+        '#6(n|adj|vb|tr|adv|intr|abbrev|pl|interj|prep|prefix|determiner|pron|conj|suffix)\.(.*)')  # nopep8
+
     @staticmethod
     def separate_def_and_pos(description):
 #        print 'searching hw in: ' + description
-        pos_and_def = re.search('#6(n|abbrev|interj)\.(.*)', description)
+        pos_and_def = CollinsParser.pos_and_def_patt.search(description)
         if pos_and_def:
-#            print 'hw found: ' + pos_and_def.group(1)
+            # print 'hw found: ' + pos_and_def.group(1)
             pos, definition = pos_and_def.group(1), pos_and_def.group(2)
         else:
-#            print 'hw not found'
+            # print 'hw not found'
             pos, definition = 'unknown', description
 
-        definition = definition.strip('.,').strip().replace('#5', '').strip(
-            '.')
+        definition = definition.strip('.,').strip().replace(
+            '#5', '').replace('#4', '').strip('.')
         definition = re.sub('^#6[^ ]*', '', definition).strip()
         definition = re.sub(' #.*', '', definition).strip()
         definition = re.sub('#1a.', '', definition).strip()
+        definition = re.sub('@m.*', '', definition).strip('.').strip()
         return pos, definition
 
 #    @staticmethod
