8bd7829b050d0e52bbb4c9ad8dd5035dae02eaab
Gabor Recski <recski@mokk.bme.hu>
2015-10-30 11:54:01 +0100

    minor

--- scripts/get_deps.py
+++ scripts/get_deps.py
@@ -3,5 +3,8 @@ import sys
 
 data = json.load(sys.stdin)
 for e in data.itervalues():
-    for dep, w1, w2 in e['senses'][0]['definition']['deps']:
-        print u"{0}\t{1}\t{2}".format(dep, w1[0], w2[0]).encode("utf-8")
+    if e['hw'] == sys.argv[1]:
+        for dep, w1, w2 in e['senses'][0]['definition']['deps']:
+            print u"{0}\t{1}\t{2}".format(dep, w1[0], w2[0]).encode("utf-8")
+
+        print e['senses'][0]['definition']['sen'].encode('utf-8')
