cca58455f3dfa44570de48bee81a5a9d30fb683d
Gabor Recski <recski@mokk.bme.hu>
2017-05-16 13:16:51 +0200

    updated scripts

--- exp/alto/tools/eval/eval.py
+++ exp/alto/tools/eval/eval.py
@@ -3,31 +3,49 @@ import sys
 
 
 def main():
-    for dirname in sys.argv[1:]:
-        fn = os.path.join(dirname, 'output')
+    total_count = 0
+    total_err = 0
+    results = []
+    dirs = os.listdir(sys.argv[1])
+    log_dir = sys.argv[2]
+    for dirname in dirs:
+        fn = os.path.join(sys.argv[1], dirname, 'output')
         lang = dirname.split('/')[-1]
         if not os.path.exists(fn):
             continue
         with open(fn) as f:
             count = 0
             err = 0
-            done = False
             for line in f:
-                if line.startswith('Done'):
-                    done = True
-                    break
                 count += 1
+                total_count += 1
                 if line.strip() == 'null':
                     err += 1
+                    total_err += 1
             if count == 0:
                 continue
-            if done:
-                print("{0} sens: {1}, parsed: {2}, coverage: {3:.0%}".format(
-                    lang, count, count-err, (count-err)/float(count)))
-            else:
-                print("{0} sens: {1}, parsed: {2}, coverage: {3:.0%}*".format(
-                    lang, count, count-err, (count-err)/float(count)))
+            coverage = (count-err) / float(count)
+            log_fn = os.path.join(log_dir, lang, 'log')
+            with open(log_fn) as lf:
+                lines = lf.readlines()
+                done = lines and lines[-1].startswith('Done')
+            results.append((coverage, count, err, lang, done))
+
+    results.sort(reverse=True)
+    print("lang\tsens\tparsed\tcoverage")
+    for coverage, count, err, lang, done in results:
+        if done:
+            print("{0}\t{1}\t{2}\t{3:.0%}".format(
+                lang, count, count-err, coverage))
+        else:
+            print("{0}\t{1}\t{2}\t{3:.0%}*".format(
+                lang, count, count-err, coverage))
+
     print("* = not done")
+
+    print("total sens: {0}, parsed: {1}, coverage: {2:.0%}".format(
+        total_count, total_count-total_err,
+        (total_count-total_err) / float(total_count)))
 
 
 if __name__ == "__main__":
--- exp/alto/tools/parse/parse.sh
+++ exp/alto/tools/parse/parse.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+# https://stackoverflow.com/questions/6593531/running-a-limited-number-of-child-processes-in-parallel-in-bash/14387296#14387296
+function max3 {
+   while [ `jobs | wc -l` -ge 3 ]
+   do
+      sleep 5
+   done
+}
+
+
+
 for dir in $@; do
     lang=`basename $dir`
     echo processing $lang
@@ -11,6 +21,8 @@ for dir in $@; do
     outdir=output/$lang
     logdir=log/$lang
     mkdir -p $outdir $logdir
-    sem -j8 nice java -cp /home/recski/projects/alto/target/alto-2.2-SNAPSHOT-jar-with-dependencies.jar de.up.ling.irtg.script.ParsingEvaluator $fn -g $grammar -I graph -o $outdir/output 2> $logdir/log &
+    echo parsing ${fn}...
+    time nice java -cp /home/recski/projects/alto/target/alto-2.2-SNAPSHOT-jar-with-dependencies.jar de.up.ling.irtg.script.ParsingEvaluator $fn -g $grammar -I graph -o $outdir/output 2> $logdir/log
+    echo done!
 done
 
