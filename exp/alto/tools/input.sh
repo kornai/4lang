a01823423a39bcc380b5e6d8314b4166570312fa
Gabor Recski <recski@mokk.bme.hu>
2017-07-11 08:41:47 +0200

    bugfix

--- exp/alto/tools/input.sh
+++ exp/alto/tools/input.sh
@@ -2,7 +2,7 @@ for dir in ../input/ud/ud-treebanks-v2.0/UD*; do
     prefix=`basename $dir`
     echo $prefix
     fn=$dir/*-train.conllu
-    python tools/dep_to_isi.py $fn > $dir/${prefix}_train.graphs
-    cat $dir/${prefix}_train.graphs | python tools/filter_large.py 25 > $dir/${prefix}_train_max25.graphs
+    python dep_to_isi.py $fn > $dir/${prefix}_train.graphs
+    cat $dir/${prefix}_train.graphs | python filter_large.py 25 > $dir/${prefix}_train_max25.graphs
 
 done
