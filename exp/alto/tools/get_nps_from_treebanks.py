c498a885725dff413645624b0c190d246e66440c
Evelin Ács <acs.evelin@gmail.com>
2018-07-27 07:45:19 +0200

    Extract only simple NPs from Penn Treebank

--- exp/alto/tools/get_nps_from_treebanks.py
+++ exp/alto/tools/get_nps_from_treebanks.py
@@ -4,5 +4,6 @@ for n in range(1,200):
     tree_file = "wsj_{}.mrg".format(str(n).zfill(4))
     sentences = treebank.parsed_sents(tree_file)
     for s in sentences:
-        for subtree in s.subtrees(lambda t: t.label().startswith("NP")):
-            print(subtree)
+        for subtree in s.subtrees(lambda t: t.label() == "NP"):
+            print(subtree.pformat(100000))
+            #print(subtree)
