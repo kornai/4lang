d64f32067c5e90123de69aef591d57ad0c44db32
Adam Kovacs <adaamko@nessie.ilab.sztaki.hu>
2019-01-25 14:15:47 +0100

    parser beta

---
+++ Reform/4lang_parser.py
@@ -0,0 +1,214 @@
+import sys, getopt
+import os
+from collections import defaultdict
+import re
+import networkx as nx
+from graphviz import Source
+from graphviz import Digraph
+import networkx as nx
+
+
+defs_to_parse = {}
+def_states = {}
+defs = {}
+
+def readfile(filename):
+    with open(filename, encoding='utf-8') as f:
+        for line in f:
+            l = line.strip().split("\t")
+            defs[l[4]] = line
+            if len(l) >= 8:
+                defs_to_parse[l[4]] = (l[0], l[7])
+                def_states[l[4]] = None
+            else:
+                def_states[l[4]] = 'err'
+
+def d_clean(string):
+        s = string
+        for c in '\\=@-,\'".!:;':
+            s = s.replace(c, '_')
+        s = s.replace('$', '_dollars')
+        s = s.replace('%', '_percent')
+        if s == '#':
+            s = '_number'
+        keywords = ("graph", "node", "strict", "edge")
+        if re.match('^[0-9]', s) or s in keywords:
+            s = "X" + s
+        return s
+
+def to_dot(graph=None):
+    lines = [u'digraph finite_state_machine {', '\tdpi=100;']
+    # lines.append('\tordering=out;')
+    # sorting everything to make the process deterministic
+    d_clean("asd")
+    node_lines = []
+    for node, n_data in graph.nodes(data=True):
+        d_node = d_clean(node)
+        printname = d_clean('_'.join(d_node.split('_')[:-1]))
+        if 'expanded' in n_data and not n_data['expanded']:
+            node_line = u'\t{0} [shape = circle, label = "{1}", \
+                    style="filled"];'.format(
+                            d_node, printname).replace('-', '_')
+        else:
+            node_line = u'\t{0} [shape = circle, label = "{1}"];'.format(
+                    d_node, printname).replace('-', '_')
+            node_lines.append(node_line)
+    lines += sorted(node_lines)
+
+    edge_lines = []
+    for u, v, edata in graph.edges(data=True):
+        if 'color' in edata:
+            d_node1 = d_clean(u)
+            d_node2 = d_clean(v)
+            edge_lines.append(
+                    u'\t{0} -> {1} [ label = "{2}" ];'.format(
+                        d_clean(d_node1), d_clean(d_node2),
+                        edata['color']))
+
+    lines += sorted(edge_lines)
+    lines.append('}')
+    return u'\n'.join(lines)
+
+def parse_def(definition, def_id):
+    nodes = definition[1].split(",")
+    G = nx.DiGraph()
+    for i, node in enumerate(nodes):
+        node = node.strip()
+        node = node.strip("<")
+        node = node.strip(">")
+        node_split = node.split()
+        node_split[0] = node_split[0].strip("<")
+        node_split[0] = node_split[0].strip(">")
+        if len(node_split) > 2:
+            def_states[def_id] = 'err'
+            return
+        if len(node_split) == 2:
+            node_split[1] = node_split[1].strip(">")
+            node_split[1] = node_split[1].strip("<")
+            if (node_split[0].isupper() & node_split[1].isupper()) | (node_split[0].isupper() & node_split[1].isupper()):
+                if node_split[0] != "'" and node_split[1] != "'":
+                    def_states[def_id] = 'err'
+                    return
+            if "/" in node_split[0]:
+                splitted = node_split[0].split("/")
+                node_split[0] = splitted[0]
+            if "/" in node_split[1]:
+                splitted = node_split[1].split("/")
+                node_split[1] = splitted[0]
+            if "(" in node_split[0]:
+                binaries = node_split[0].split("(")
+                if binaries[0] == '':
+                    def_states[def_id] = 'err'
+                    return
+                if len(binaries[1].split()) > 1 or len(binaries[0].split()) > 1:
+                    def_states[def_id] = 'err'
+                    return
+                if len(binaries) > 2 or binaries[0].isupper() or binaries[1].isupper():
+                    def_states[def_id] = 'err'
+                    return
+                binaries[1] = binaries[1].strip(")")
+                node_split[0] = binaries[1]
+                G.add_edge(binaries[1] + "_" + str(i), binaries[0] + "_" + str(i), color = 0)
+            if "(" in node_split[1]:
+                binaries = node_split[1].split("(")
+                if binaries[0] == '':
+                    def_states[def_id] = 'err'
+                    return
+                if len(binaries[1].split()) > 1 or len(binaries[0].split()) > 1:
+                    def_states[def_id] = 'err'
+                    return
+                if len(binaries) > 2 or binaries[0].isupper() or binaries[1].isupper():
+                    def_states[def_id] = 'err'
+                    return
+                binaries[1] = binaries[1].strip(")")
+                node_split[1] = binaries[1]
+                G.add_edge(binaries[1] + "_" + str(i), binaries[0] + "_" + str(i), color = 0)
+            if node_split[0].isupper() and node_split[0] != "'":
+                if node_split[1] == "'":
+                    G.add_edge(node_split[0] + "_" + str(i), definition[0] + "_", color = 1)
+                else:
+                    G.add_edge(node_split[0] + "_" + str(i), definition[0] + "_", color = 1)
+                    G.add_edge(node_split[0] + "_" + str(i), node_split[1] + "_", color = 2)
+            if node_split[1].isupper() and node_split[1] != "'":
+                if node_split[0] == "'":
+                    G.add_edge(node_split[1] + "_" + str(i), definition[0] + "_", color = 2)
+                else:
+                    G.add_edge(node_split[1] + "_" + str(i), definition[0] + "_", color = 2)
+                    G.add_edge(node_split[1] + "_" + str(i), node_split[0] + "_", color = 1)
+                
+        if len(node_split) == 1:
+            if node_split[0].isupper():
+                def_states[def_id] = 'err'
+                return
+            if "/" in node_split[0]:
+                node_split[0] = node_split[0].split("/")[0]      
+            if "(" in node_split[0]:
+                binaries = node_split[0].split("(")
+                if len(binaries[1].split()) > 1 or len(binaries[0].split()) > 1:
+                    def_states[def_id] = 'err'
+                    return
+                if binaries[0] == '':
+                    def_states[def_id] = 'err'
+                    return
+                if len(binaries) > 2 or binaries[0].isupper() or binaries[1].isupper():
+                    def_states[def_id] = 'err'
+                    return
+                binaries[1] = binaries[1].strip(")")
+                node_split[0] = binaries[1]
+                G.add_edge(binaries[1] + "_" + str(i), binaries[0] + "_" + str(i), color = 0)
+            G.add_edge(definition[0] + "_", node_split[0] + "_", color = 0)
+    return G
+
+def filter_def(definition, def_id):
+    pat = re.search(r".*=.*|\[(.+)\]", definition[1])
+    if not definition[1]:
+        def_states[def_id] = 'err'
+    elif pat:
+        def_states[def_id] = 'err'
+    else:
+        print(definition)
+        G = parse_def(definition, def_id)
+        if G is not None:
+            d = to_dot(G)
+            return d
+        return None
+
+def process(outputdir):
+    for element in defs_to_parse:
+        d = filter_def(defs_to_parse[element], element)
+        if d is not None:
+            with open(outputdir  + defs_to_parse[element][0] + "_" + element + ".dot", "w+") as o:
+                print(d, file=o)
+                
+def main(argv):
+    inputfile = ''
+    outputdir = ''
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","odir="])
+    except getopt.GetoptError:
+        print('4lang_parser.py -i <inputfile> -o <outputdir>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('4lang_parser.py -i <inputfile> -o <outputdir>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--odir"):
+            outputdir = arg
+    f = inputfile
+    print(inputfile)
+    readfile(f)
+    o = outputdir
+    process(o)
+    errors = []
+    for state in def_states:
+        if def_states[state] == 'err':
+            errors.append(defs[state])
+    errors.sort()
+    with open(outputdir + "4lang_def_errors", 'w', encoding="utf-8") as f:
+        for item in errors:
+            f.write("%s" % item)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
