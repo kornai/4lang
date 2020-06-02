4a4faf399a1770b674560edc526df53745dd7ff0
Kovacs Adam <adaam.ko@gmail.com>
2020-06-02 11:55:21 +0200

    Remove defined

--- Reform/remove_defined.py
+++ Reform/remove_defined.py
@@ -1,12 +1,14 @@
 import os
 import re
-import sys, getopt
+import sys
+import getopt
 from collections import defaultdict
+
 
 def main(argv):
     inputfile = ''
     try:
-        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","odir="])
+        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "odir="])
     except getopt.GetoptError:
         print('def_ply_parser.py -i <inputfile> -o <outputdir>')
         sys.exit(2)
@@ -17,14 +19,27 @@ def main(argv):
         elif opt in ("-i", "--ifile"):
             inputfile = arg
 
-    w = defaultdict(int)
-    concepts = []
+    lines = []
     with open(inputfile) as f:
         for line in f:
+            lines.append(line.strip("\n"))
+
+    found = True
+    while found:
+        to_del_index = None
+        concepts = []
+        for line in lines:
             if not line.startswith("%"):
+                concept = line.split("\t")[0].strip() + \
+                    "_" + line.split("\t")[4]
                 concepts.append(line.split("\t")[0].strip())
-                if len(line.split("\t")) > 8:
+
+        for i, line in enumerate(lines):
+            if not line.startswith("%"):
+                if len(line.split("\t")) >= 8:
                     line = line.split("\t")[7].split("%")[0]
+                    line = re.sub("@", "", line)
+                    line = re.sub('"', "", line)
                     line = re.sub(",", " ", line)
                     line = re.sub("\{", " ", line)
                     line = re.sub("\}", " ", line)
@@ -35,23 +50,23 @@ def main(argv):
                     line = re.sub("[0-9]*", "", line)
                     line = re.sub("/", "", line)
                     words = line.split()
+                    w_keys = []
                     for wo in words:
                         wo = wo.strip()
                         if wo and not wo.startswith("=") and not wo.startswith('"') and not wo.isupper() and not ">" in wo and not "<" in wo:
-                            w[wo.strip()] += 1
+                            w_keys.append(wo.strip())
+                    inter = set(concepts) & set(w_keys)
+                    if len(inter) == len(set(w_keys)):
+                        to_del_index = i
+                        break
+        if to_del_index:
+            lines = lines[:to_del_index] + lines[to_del_index+1:]
+        else:
+            found = False
 
-    w_keys = w.keys()
-    inter = set(concepts) & set(w_keys)
-    not_in = {}
-
-    for key in w:
-        if key not in inter:
-            not_in[key] = w[key]
-
-    t = sorted(not_in.items(), key=lambda x: x[1])
-
-    for elem in t[::-1]:
-        print(str(elem[0]) + "\t" + str(elem[1]))
+    if not found:
+        for line in lines:
+            print(line)
 
 
 if __name__ == "__main__":
